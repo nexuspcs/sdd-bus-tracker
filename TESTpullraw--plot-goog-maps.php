4994e6e4a7157a7e562bebffa2f13411e21bc316
npcs <69493073+nexuspcs@users.noreply.github.com>
2023-04-12 19:45:38 +1000

    debugging

--- TESTpullraw--plot-goog-maps.php
+++ TESTpullraw--plot-goog-maps.php
@@ -1,50 +1,44 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Bus Positions Map</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Bus Locations</title>
+	<!-- Load Leaflet from CDN -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/leaflet/1.3.1/leaflet.css" />
+	<script src="https://cdn.jsdelivr.net/leaflet/1.3.1/leaflet.js"></script>
 </head>
-<body onload="initMap()">
-    <?php
-    // Perform the curl request and display the map
-    $curl = curl_init();
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => "https://api.transport.nsw.gov.au/v1/gtfs/vehiclepos/buses?debug=true",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_HTTPHEADER => array(
-            "Accept: text/plain",
-            "Authorization: apikey m5uvuKOQtsngacQIQemt0LqzC8Xq7nVxECVp"
-        ),
-    ));
-    $response = curl_exec($curl);
-    var_dump($response);
-    curl_close($curl);
+<body>
+	<!-- Create a map container with a specified height -->
+	<div id="mapid" style="height: 500px;"></div>
 
-    $data = json_decode($response, true);
-    ?>
+	<script>
+		// Create a Leaflet map centered at Sydney with zoom level 12
+		var mymap = L.map('mapid').setView([-33.865, 151.209], 12);
 
-    <div id="map" style="height: 400px;">Map could not be loaded.</div>
-    <script>
-        function initMap() {
-            alert("initMap")
-            var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 12,
-                center: {lat: -33.865143, lng: 151.209900} // Sydney CBD
-            });
+		// Load the OpenStreetMap tiles from the CDN
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			maxZoom: 19,
+			attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
+		}).addTo(mymap);
 
-            // Add a marker for each bus position
-            var busPositions = <?php echo json_encode($data['entity']); ?>;
-            for (var i = 0; i < busPositions.length; i++) {
-                var bus = busPositions[i].vehicle;
-                var marker = new google.maps.Marker({
-                    position: {lat: bus.position.latitude, lng: bus.position.longitude},
-                    map: map,
-                    title: bus.trip.route_id + ' - ' + bus.trip.trip_headsign
-                });
-            }
-        }
-    </script>
-    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAysQG08FsOhtF0XyWN2UfjrpzHCQmsO6A&callback=initMap"></script>
+		// Perform the curl request and display the buses on the map
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", "https://api.transport.nsw.gov.au/v1/gtfs/vehiclepos/buses?debug=true");
+		xhr.setRequestHeader("Accept", "application/json");
+		xhr.setRequestHeader("Authorization", "apikey m5uvuKOQtsngacQIQemt0LqzC8Xq7nVxECVp");
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState === 4 && xhr.status === 200) {
+				var data = JSON.parse(xhr.responseText);
+				// Get the latitude and longitude for the first 10 buses
+				var buses = data.entity.slice(0, 10).map(function(entity) {
+					return [entity.vehicle.position.latitude, entity.vehicle.position.longitude];
+				});
+				// Add the bus markers to the map
+				for (var i = 0; i < buses.length; i++) {
+					L.marker(buses[i]).addTo(mymap);
+				}
+			}
+		};
+		xhr.send();
+	</script>
 </body>
 </html>
