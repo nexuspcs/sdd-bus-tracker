cf6af7bfa9e0f0efc8dd6efaff25a4ca16f2bcb7
npcs <69493073+nexuspcs@users.noreply.github.com>
2023-05-09 09:23:41 +1000

    Updating dirs

--- depBoardTut/tableheadlandquirketc--and-nearestbusattop.php
+++ depBoardTut/tableheadlandquirketc--and-nearestbusattop.php
@@ -5,32 +5,42 @@
     <title>SLGS Bus Tracker</title>
 
     <style>
-        .bus-info {
-            color: gold;
-            font-weight: bold;
-            display: none;
-        }
-
-        table {
-            border-collapse: collapse;
-            width: 60%;
-            margin-left: auto;
-            margin-right: auto;
-            margin-bottom: 20px;
-        }
-
-        th,
-        td {
-            border: 1px solid black;
-            text-align: left;
-            padding: 8px;
-        }
-
-        th {
-            background-color: #f2f2f2;
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f5f5f5;
+            margin: 0;
+            padding: 0;
         }
 
         .welcomeTitle {
+            text-align: center;
+            background-color: #3b5998;
+            color: white;
+            padding: 20px;
+            font-size: 24px;
+            margin-bottom: 20px;
+        }
+
+        .currentDateTime {
+            text-align: center;
+            font-weight: bold;
+            margin-bottom: 20px;
+        }
+
+        .bus-container {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            margin-bottom: 20px;
+        }
+
+        .bus-card {
+            background-color: white;
+            width: 250px;
+            padding: 15px;
+            margin: 10px;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+            border-radius: 4px;
             text-align: center;
         }
 
@@ -40,10 +50,14 @@
             color: blue;
         }
 
-        .currentDateTime {
-            text-align: center;
+        .bus-destination {
+            font-size: 1.1em;
+            margin-bottom: 10px;
+        }
+
+        .bus-time {
             font-weight: bold;
-            margin-bottom: 20px;
+            font-size: 1.5em;
         }
     </style>
 
@@ -61,15 +75,12 @@
     <script>
         function displayNearestBus(nearestBus) {
             if (nearestBus !== null) {
-                const hours = nearestBus.timeInMins >= 60 ? Math.floor(nearestBus.timeInMins / 60) : 0;
-
-                const remainingMinutes = nearestBus.timeInMins % 60;
                 const nearestBusInfo = document.getElementById("nearestBusInfo");
-                nearestBusInfo.innerText = `Nearest Bus: ${nearestBus.routeInfo} in ${hours > 0 ? hours + 'h ' : ''}${remainingMinutes}m`;
-
+                nearestBusInfo.innerText = `Nearest Bus: ${nearestBus.routeInfo} in ${nearestBus.timeInMins}m`;
                 nearestBusInfo.style.display = "block";
             }
         }
+
         const refreshDelay = 5000; // Refreshes and pulls new data from API every x milliseconds
         var countMulpt = 0;
         var refreshDelayCounterSECONDS = 0;
@@ -83,6 +94,8 @@
                     const busRows = busData.querySelectorAll("tr");
                     let nearestBus = null;
 
+                    let busCardsHTML = "";
+
                     for (let row of busRows) {
                         const routeInfo = row.cells[0].innerText;
                         const timeInfo = row.cells[1].innerText;
@@ -90,7 +103,7 @@
                         const timeMatch = timeInfo.match(timeRegex);
 
                         if (timeMatch !== null) {
-                            let timeInMins = parseInt(timeMatch[1]) * 60;
+                            let timeInMins = parseInt(timeMatch[1]) * 1;
 
                             if (timeMatch[2]) {
                                 timeInMins += parseInt(timeMatch[2]);
@@ -102,8 +115,26 @@
                                     timeInMins,
                                 };
                             }
+
+                            const routeNumber = routeInfo.match(/\d+/)[0];
+                            const destination = routeInfo.replace(/^\d+/, "").trim();
+                            const hours = timeInMins >= 60 ? Math.floor(timeInMins / 60) : 0;
+                            const remainingMinutes = timeInMins % 60;
+                            const timeStrHRMIN = hours > 0 ? hours + 'h ' + remainingMinutes + 'm' : remainingMinutes + 'm';
+
+
+                            busCardsHTML += `<div class="bus-card">
+    <div class="route-number">${routeNumber}</div>
+    <div class="bus-destination">${destination}</div>
+    <div class="bus-time">${timeInMins}m</div>
+</div>`;
+
+
                         }
                     }
+
+                    busData.innerHTML = `<div class="bus-container">${busCardsHTML}</div>`;
+
 
                     displayNearestBus(nearestBus);
                 }
@@ -139,7 +170,7 @@
         $apiEndpoint = 'https://api.transport.nsw.gov.au/v1/tp/'; // First define the API endpoint, which is the base URL of the API. This is the same for all API calls.
         $apiCall = 'departure_mon';
         $when = time(); // Now
-        $stopIds = array("209926", "209927"); // Replace with the desired stop ID (testing stop id, is qvb, york st;; 200041) (headland rd slgs stop id is; 209926;;;;;;;  quirk st; 209927) (mona bline; 210323)
+        $stopIds = array("2000133", ""); // Replace with the desired stop ID (testing stop id, is qvb, york st;; 200041) (headland rd slgs stop id is; 209926;;;;;;;  quirk st; 209927) (mona bline; 210323)
         $stop = "";
         $retryAttempts = 3; // Next define the number of retry attempts for the API call. This is the number of times that the code will try to get data from the API before returning a failure.
         $retryDelay = 0; // A delay (in seconds) for how long the request will 'hang' while waiting for data back from the API
@@ -205,11 +236,13 @@
 
                         $hours = floor($minutes / 60);
                         $remainingMinutes = $minutes % 60;
-                        $timeStr = $hours > 0 ? $hours . 'h ' . $remainingMinutes . 'm' : $remainingMinutes . 'm';
+                        $timeStr = $minutes . 'm';
+
+
 
 
                         echo "<tr>";
-                        echo "<td>" . "<span class='route-number'>" . $routeNumber . "</span>" . " to " . $destination . "</td>";
+                        echo "<td>" . "<span class='route-number'>" . $routeNumber . "</span>" . " to " . $destination . " (from " . $location['name'] . ")" . "</td>";
                         echo "<td>" . $timeStr . "</td>";
                         echo "</tr>";
                     }
