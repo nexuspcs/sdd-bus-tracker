9c54c9af1482035db1b4cbf36b502204cea10313
npcs <69493073+nexuspcs@users.noreply.github.com>
2023-05-09 21:40:59 +1000

    Scales on iPhone app now!

--- depBoardTut/tableheadlandquirketc--and-nearestbusattop.php
+++ depBoardTut/tableheadlandquirketc--and-nearestbusattop.php
@@ -3,7 +3,7 @@
 
 <head>
     <title>SLGS Bus Tracker</title>
-
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
     <style>
         body {
             font-family: Arial, sans-serif;
